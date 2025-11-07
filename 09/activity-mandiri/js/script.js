cc9d3915a3254a508638fc5cd5ecea56b2b27fc7
Kadmiel Kislew Stevano Karenda <ringt904@gmail.com>
2025-11-07 14:58:06 +0700

    Add files via upload

---
+++ 09/activity-mandiri/js/script.js
@@ -0,0 +1,26 @@
+function fn_ValForm() {
+  var sMsg = "";
+  var name = document.getElementById("name").value.trim();
+  var email = document.getElementById("email").value.trim();
+  var message = document.getElementById("message").value.trim();
+  var emailPattern = /^[a-z0-9][a-z0-9_\.-]{0,}[a-z0-9]@[a-z0-9][a-z0-9_\.-]{0,}[a-z0-9]\.[a-z0-9]{2,4}$/;
+
+  if (name === "") {
+    sMsg += "\n* Anda belum mengisikan nama";
+  }
+  if (email === "") {
+    sMsg += "\n* Anda belum mengisikan email";
+  } else if (!emailPattern.test(email)) {
+    sMsg += "\n* Format email tidak valid";
+  }
+  if (message === "") {
+    sMsg += "\n* Anda belum mengisikan pesan";
+  }
+
+  if (sMsg !== "") {
+    alert("Peringatan:\n" + sMsg);
+    return false;
+  } else {
+    return true;
+  }
+}
